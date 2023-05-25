e18d9b26070878acd0fffc33e617f413e36e3533
Jhonn <jhonnsalenss@gmail.com>
2023-05-24 20:44:16 -0400

    fix

--- imc.py
+++ imc.py
@@ -1,4 +1,4 @@
-h = float(input('Cual es su altura? '))
-p = float(input('Cual es su peso? '))
-imc = (p / (h ** 2))
+h = float(input('Cual es su altura en metros? '))
+p = float(input('Cual es su peso en kg? '))
+imc = round((p / (h ** 2)),2)
 print('Tu indice de masa corporal es: ',imc)
